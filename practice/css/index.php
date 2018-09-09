6654d7c72ffe99257b0716402915b788a475126c
Jose Fausto <jofausto@csumb.edu>
2018-09-09 21:55:45 +0000

    added css precedence testing

---
+++ practice/css/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>CSS Examples</title>
+        <link href="/practice/css/sheet1.css" rel="stylesheet">
+    </head>
+    <body>
+        <style>
+            p{
+                margin:"10px";
+            }
+        </style>
+        <p>
+            Chole Gang
+        </p>
+        <p class="highlighted">
+            Gang
+        </p>
+        <p id="first">
+            Gang
+        </p>
+        
+        <img src="https://www.thesoccerstore.com/media/catalog/product/cache/2/image/500x500/9df78eab33525d08d6e5fb8d27136e95/c/h/chivas_1_2.jpg"style="width:150px">
+        <style>
+            img{
+                width:400px;
+            }
+        </style>
+    </body>
+</html>
