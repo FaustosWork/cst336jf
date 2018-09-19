38701df528ed35519e250c59a023df6a8fb9f059
Jose Fausto <jofausto@csumb.edu>
2018-09-19 22:49:41 +0000

    lab 2 incoming

--- index.php
+++ index.php
@@ -1,13 +1,9 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title> Here </title>
+        <title> Index </title>
     </head>
     <body>
-<<<<<<< HEAD
         You are here
-=======
-        You are here. 
->>>>>>> ae7684d5f28580f137655aa60f9623d3a0aade91
     </body>
 </html>
--- labs/777/777/index.php
+++ labs/777/777/index.php
@@ -1,9 +1,44 @@
+<?php
+            
+include 'inc/functions.php';
+            
+?>
 <!DOCTYPE html>
 <html>
     <head>
         <title> 777 Slot Machine </title>
+        <style>
+            @import url("css/styles.css");
+        </style>
     </head>
     <body>
-        <img src="img/cherry.png" alt="cherry" title="Cherry" width="70" />
+        <?php
+        
+        //     for ($i=1; $i<4; $i++){
+        //         ${"randomValue" . $i} = rand(0,2);
+        //         displaySymbol(${"randomValue" . $i});
+        //     }
+            
+            
+        //     for ($i=1; $i<4; $i++){
+        //         ${"randomValue" . $i } = rand(1,1);
+        //         displaySymbol(${"randomValue" . $i});
+        //     }
+        //     displayPoints($randomValue1, $randomValue2, $randomValue3);
+        
+        ?>        
+        
+        <div id="main">
+            <?php
+                play();
+            ?>
+        
+            <form>
+                <input type="submit" value="Spin!"/>
+            </form>
+        </div>
+        
+        
+        
     </body>
 </html>
