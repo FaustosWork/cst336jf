944f7e15bf12f28e0445b95e744d80a5b6b1aa16
Jose Fausto <jofausto@csumb.edu>
2018-11-17 03:40:22 +0000

    added unfinished hangman lab

--- examples/meme_lab/meme.php
+++ examples/meme_lab/meme.php
@@ -53,9 +53,9 @@ function displayMemes() {
     
     $sql = "SELECT * from all_memes WHERE 1";  
     
-    echo "POST: "; 
-    print_r($_POST);
-    echo "<br/>"; 
+    //echo "POST: "; 
+    //print_r($_POST);
+    //echo "<br/>"; 
     
     if(isset($_POST['search']) && !empty($_POST['search'])) {
       // query the databse for any records that match this search
