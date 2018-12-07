9656611d25fd92ffd94428ddf3f11b0b4e15b61f
Jose Fausto <jofausto@csumb.edu>
2018-12-07 23:27:29 +0000

    fixed up lab 9

--- labs/ajax/functions.js
+++ labs/ajax/functions.js
@@ -1,203 +1,84 @@
-const ZIP_URL = 'https://www.showdeolabs.com/cors?url=http://itcdland.csumb.edu/~milara/ajax/cityInfoByZip.php?zip=';
-const COUNTY_URL = 'https://www.showdeolabs.com/cors?url=http://itcdland.csumb.edu/~milara/ajax/countyList.php?state=';
-
-window.onload = initialize;
-
-let selects;
-function initialize() {
+$("#zipcode").change(zipcodeChange);
+$("#states").change(stateChange);
+$("#username").change(userNameChange);
     
-    selects = document.querySelectorAll('select');
-    
-    loadStates();
-    initializeSelect();
-    initializeZip();
-    
-    document.querySelector('#confirm').addEventListener('click', validateForm);
+function stateChange(){
+    $.ajax({
+            type: "get",
+            url: "https://www.showdeolabs.com/cors?url=http://itcdland.csumb.edu/~milara/ajax/countyList.php?state=" + $("#states").val(),
+            dataType: "json",
+            data: {
+                "format":"geojson",
+            },
+            
+            success: function(data,status) {
+                var realData = JSON.parse(data);
+                var select = document.getElementById("counties");
+                select.options.length = 1;
+  
+                for (index in realData) {
+                   select.options[select.options.length] = new Option(realData[index]['county'], index);
+                 }
+            }
+    });
+      
 }
-
-function initializeSelect() {
-    M.FormSelect.init(selects, {});
-}
-
-function initializeZip() {
     
-    document.querySelector('#zip-code').addEventListener('keyup', e => {
-        setZipData(e.target.value);
+function zipcodeChange() {      
+    $.ajax({
+            type: "get",
+            url: "https://www.showdeolabs.com/cors?url=http://itcdland.csumb.edu/~milara/ajax/cityInfoByZip.php?zip=" + $("#zipcode").val(),
+            dataType: "json",
+            data: {
+                "format":"geojson",
+            },
+            
+            success: function(data,status) {
+                var realData = JSON.parse(data);
+                
+                for (var i=0; i < (Object.keys(realData).length / 2); i++ ) {
+                    $('#zipcodeResult').append(realData[i] + "<br>");
+                }
+                 
+                if(Object.keys(realData).length == 0){
+                    document.getElementById("msg").innerHTML = " Zipcode not found";
+                }
+                else{
+                    document.getElementById("msg").innerHTML = "";
+                    document.myForm.city.value = realData[1];
+                    document.myForm.latitude.value = realData[5];
+                    document.myForm.longitude.value = realData[6];
+                }
+            }
     });
 }
 
-async function setZipData(zip) {
-    const zipCall = await fetch(`${ZIP_URL}${zip}`, {
-        method: 'GET',
-        headers:{ 'Content-Type': 'application/json'}
-    });
+function userNameChange(){
+    var found = false;
     
-    // fetch response
-    const response = await zipCall.json();
-    
-    // make resonse JSON data
-    const data = JSON.parse(response);
-
-    // set data
-    const city = document.querySelector('#city');
-    const lat = document.querySelector('#lat');
-    const lng = document.querySelector('#lng');
-    if (data) {
-        city.innerHTML = data.city;
-        lat.innerHTML = `Lat: ${data.latitude}`;
-        lng.innerHTML = `Lat: ${data.longitude}`;
-    }
-    
-    else {
-        city.innerHTML = 'No area found...';
-        lat.innerHTML = '';
-        lng.innerHTML = '';
-    }
+    for(name in usernames){
+        if($("#username").val() == usernames[name]){
+          found = true;
+        }
+      }
+      
+      if(found != true){
+        document.getElementById("confirmation").innerHTML = "Username Available";
+        document.getElementById("confirmation").className = "confirmed";
+      }else{
+        document.getElementById("confirmation").innerHTML = "Username Taken";
+        document.getElementById("confirmation").className = "error";
+      }
 }
 
-function loadStates() {
-    
-    const STATES = {
-        "AL": "Alabama",
-        "AK": "Alaska",
-        "AS": "American Samoa",
-        "AZ": "Arizona",
-        "AR": "Arkansas",
-        "CA": "California",
-        "CO": "Colorado",
-        "CT": "Connecticut",
-        "DE": "Delaware",
-        "DC": "District Of Columbia",
-        "FM": "Federated States Of Micronesia",
-        "FL": "Florida",
-        "GA": "Georgia",
-        "GU": "Guam",
-        "HI": "Hawaii",
-        "ID": "Idaho",
-        "IL": "Illinois",
-        "IN": "Indiana",
-        "IA": "Iowa",
-        "KS": "Kansas",
-        "KY": "Kentucky",
-        "LA": "Louisiana",
-        "ME": "Maine",
-        "MH": "Marshall Islands",
-        "MD": "Maryland",
-        "MA": "Massachusetts",
-        "MI": "Michigan",
-        "MN": "Minnesota",
-        "MS": "Mississippi",
-        "MO": "Missouri",
-        "MT": "Montana",
-        "NE": "Nebraska",
-        "NV": "Nevada",
-        "NH": "New Hampshire",
-        "NJ": "New Jersey",
-        "NM": "New Mexico",
-        "NY": "New York",
-        "NC": "North Carolina",
-        "ND": "North Dakota",
-        "MP": "Northern Mariana Islands",
-        "OH": "Ohio",
-        "OK": "Oklahoma",
-        "OR": "Oregon",
-        "PW": "Palau",
-        "PA": "Pennsylvania",
-        "PR": "Puerto Rico",
-        "RI": "Rhode Island",
-        "SC": "South Carolina",
-        "SD": "South Dakota",
-        "TN": "Tennessee",
-        "TX": "Texas",
-        "UT": "Utah",
-        "VT": "Vermont",
-        "VI": "Virgin Islands",
-        "VA": "Virginia",
-        "WA": "Washington",
-        "WV": "West Virginia",
-        "WI": "Wisconsin",
-        "WY": "Wyoming"
-    };
-    
-    // create option for each state
-    const stateSelect = Array.from(selects)[0];
-    Object.keys(STATES).forEach(key => {
-       createOption(key, STATES[key], stateSelect);
-    });
-    
-    // listen for state change
-    stateSelect.addEventListener('change', setCounty);
-
-    // dispatch event change
-    stateSelect.dispatchEvent(new Event('change'));
-}
-
-async function setCounty(e) {
-    
-    const countyCall = await fetch(`${COUNTY_URL}${e.target.value}`, {
-        method: 'GET',
-        headers:{ 'Content-Type': 'application/json'}
-    });
-    
-    // fetch response
-    const response = await countyCall.json();
-    
-    // make resonse JSON data
-    const data = JSON.parse(response);
-    
-    // create county options
-    const countySelect = Array.from(selects)[1];
-    clearSelect(countySelect); // clear select before creating new options
-    
-    if (data.length !== 0) {
-        data.map(obj => {
-            createOption(obj.county, obj.county, countySelect); 
-        });
+function submitCheck(){
+    if($("#password").val()==$("#password2").val()){
+        document.getElementById("passConfirm").innerHTML = "Passwords Match";
+        document.getElementById("passConfirm").className = "confirmed";
+        return true;
+    }else{
+        document.getElementById("passConfirm").innerHTML = "Passwords Do Not Match!";
+        document.getElementById("passConfirm").className = "error";
+        return false;
     }
-    
-    else {
-        createOption('-1', 'No County', countySelect); 
-    }
-    
-    initializeSelect();
-}
-
-function clearSelect(select) {
-    
-    Array.from(select.children).forEach(child => { 
-        select.removeChild(child); 
-    });
-}
-
-function createOption(value, text, parent) {
-    
-    // create option element
-    const option = document.createElement('option');
-    option.value = value;
-    option.innerHTML = text;
-    
-    // append option element to parent
-    parent.appendChild(option);
-}
-
-function validateForm() {
-    
-    const passwords = Array.from(document.querySelectorAll('input[type="password"]'));
-    const formStatus = document.querySelector('#form-status');
-    formStatus.className = 'error';
-    
-    if (passwords[0].value !== passwords[1].value) {
-        formStatus.innerHTML = 'Passwords do not match';
-        return;
-    }
-    
-    if (document.querySelector('#username').value.length < 10) {
-        formStatus.innerHTML = 'Username is not available';
-        return;
-    }
-    
-    // form was posted
-    formStatus.className = 'success';
-    formStatus.innerHTML = 'Account successfully created';
-    
 }
--- labs/ajax/index.php
+++ labs/ajax/index.php
@@ -1,52 +1,98 @@
-<!DOCTYPE html>
 <html>
-    <head> 
-       
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-        <script type="text/javascript" src="public/js/index.js" type= "module"></script>
-    </head>
+    <head>
+
+    <title> Ajax Signup </title>
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
     
-    <body>
-        <main class="container row align center">
-            <h1>Sign Up</h1>
-             <form class="col l8 offset-l2">
-                <div class="row">
-                    <div class="input-field col s6">
-                      <input placeholder="Username" id="username" type="text">
-                    </div>
-                    
-                    <div class="input-field col s6">
-                      <input placeholder="ZIP Code" id="zip-code" type="number">
-                    </div>
-                    
-                    <div class="input-field col s6">
-                        <select>
-                        </select>
-                    </div>
-                    
-                    <div class="input-field col s6">
-                        <select>
-                        </select>
-                    </div>
-                    
-                    <div class="input-field col s6">
-                      <input placeholder="Password" id="password" type="password">
-                    </div>
-                    <div class="input-field col s6">
-                      <input placeholder="Confirm Password" id="confirm-password" type="password">
-                    </div>
-                </div>
-                <br>
-                <p id="form-status" class="error"></p>
-                <br>
-                <a id="confirm" class="waves-effect waves-light btn">Press this useless button to continue</a>
-            </form>
-            <div id="city-cont" class="col s12">
-                <h3 id="city"></h3>
-                <h5 id="lat-lng"><span id="lat"></span> <span id="lng"></span></h5>
-            </div>
-        </main>
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    
+    <script> var usernames = ["bob"]</script>
+
+    </head>
+
+    <body background="img/monterey.jpg">
+
+        <header>
+            <h1>Signup</h1>
+        </header>
+
+        <form method="post" name="myForm" onsubmit= "return submitCheck()">
+        Username:   <input type="text" id="username"> <span id="confirmation"></span> <br><br>
+        Password:   <input type="password" id="password">  <span id="passConfirm"></span><br><br>
+        Retype Password:   <input type="password" id="password2"> <br><br>
+        Zip Code:     <input type="text" id="zipcode" name="zipcode"><span id="msg"></span> <br><br>
+        City: <input type="text" name="city" id="city"> <br><br>
+        Longitude: <input type="text" name="longitude" id="longitude"> <br><br>
+        Latitude: <input type="text" name="latitude" id="latitude"> <br><br>
+        State: <select id = "states">
+            <option value="default"></option>
+            <option value="AL">Alabama</option>
+            <option value="AK">Alaska</option>
+            <option value="AZ">Arizona</option>
+            <option value="AR">Arkansas</option>
+            <option value="CA">California</option>
+            <option value="CO">Colorado</option>
+            <option value="CT">Connecticut</option>
+            <option value="DE">Delaware</option>
+            <option value="DC">District Of Columbia</option>
+            <option value="FL">Florida</option>
+            <option value="GA">Georgia</option>
+            <option value="HI">Hawaii</option>
+            <option value="ID">Idaho</option>
+            <option value="IL">Illinois</option>
+            <option value="IN">Indiana</option>
+            <option value="IA">Iowa</option>
+            <option value="KS">Kansas</option>
+            <option value="KY">Kentucky</option>
+	        <option value="LA">Louisiana</option>
+	        <option value="ME">Maine</option>
+	        <option value="MD">Maryland</option>
+	        <option value="MA">Massachusetts</option>
+	        <option value="MI">Michigan</option>
+	        <option value="MN">Minnesota</option>
+	        <option value="MS">Mississippi</option>
+	        <option value="MO">Missouri</option>
+	        <option value="MT">Montana</option>
+	        <option value="NE">Nebraska</option>
+	        <option value="NV">Nevada</option>
+	        <option value="NH">New Hampshire</option>
+	        <option value="NJ">New Jersey</option>
+	        <option value="NM">New Mexico</option>
+	        <option value="NY">New York</option>
+	        <option value="NC">North Carolina</option>
+	        <option value="ND">North Dakota</option>
+	        <option value="OH">Ohio</option>
+	        <option value="OK">Oklahoma</option>
+	        <option value="OR">Oregon</option>
+	        <option value="PA">Pennsylvania</option>
+	        <option value="RI">Rhode Island</option>
+	        <option value="SC">South Carolina</option>
+	        <option value="SD">South Dakota</option>
+	        <option value="TN">Tennessee</option>
+	        <option value="TX">Texas</option>
+	        <option value="UT">Utah</option>
+	        <option value="VT">Vermont</option>
+	        <option value="VA">Virginia</option>
+	        <option value="WA">Washington</option>
+	        <option value="WV">West Virginia</option>
+	        <option value="WI">Wisconsin</option>
+        	<option value="WY">Wyoming</option>
+            
+            </select>	<br><br>	
+            
+            Counties: <select id="counties">
+                <option value="default"></option>
+            </select>
+        
+            <input type="submit" id="submit" class="submit">
+
+        </form>
+
+        <div id="zipcodeResult"></div> Results
+
+        <script src="functions.js"></script>
+
     </body>
     
 </html>
